d23240cbdd159ea3a227183a4c238c4985d01673
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2025-06-09 15:36:28 +0700

    selesai

--- ioFile.cpp
+++ ioFile.cpp
@@ -18,4 +18,34 @@ int main() {
         cout << "- ";
         //mendapatkan setiap karakter dalam satu baris 
         getline(cin,baris);
-       
+        //loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukan nilai dari 'baris' ke dalam file 
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //membuat objek input file stream
+    ifstream infile;
+    //memnbuka file yang dituliskan 
+    infile.open ("contohfile.txt");
+
+    cout << endl << ">= membuka dan membaca file " << endl;
+    //jika file ada maka 
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan di sini 
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak di temukan file maka akan menampilkan ini
+    else cout << "unable to open file ";
+    return 0;
+
+}
