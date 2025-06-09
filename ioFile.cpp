6342e776b26dbfc89f4a826acd6eea018d491202
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2025-06-09 14:51:39 +0700

    membuat int main

--- ioFile.cpp
+++ ioFile.cpp
@@ -4,11 +4,4 @@
 using namespace std;
 
 int main() {
-    string baris;
-
-    //membuat objek output file stream.
-    ofstream outfile;
-    //membuka file untuk ditulisi.
-    outfile.open("contohfile.txt");
-
-    
+   
