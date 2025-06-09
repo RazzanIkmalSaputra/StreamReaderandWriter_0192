8c5d2cd15b35b73b8903c44ac214df44533117ec
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2025-06-09 14:53:50 +0700

    jika tidak ditemukan file maka akan menampilkan ini

--- ioFile.cpp
+++ ioFile.cpp
@@ -4,4 +4,48 @@
 using namespace std;
 
 int main() {
-   
+    string baris;
+
+    //membuat objek output file stream.
+    ofstream outfile;
+    //membuka file untuk ditulisi.
+    outfile.open("contohfile.txt");
+
+    cout << ">= menulis file, \'q\'untuk keluar" << endl;
+
+    //unlimited loop untuk menulis 
+    while (true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris 
+        getline(cin,baris);
+        //loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukan nilai dari 'baris' ke dalam file 
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //membuat objek input file stream
+    ifstream infile;
+    //memnbuka file yang dituliskan 
+    infile.open ("contohfile.txt");
+
+    cout << endl << ">= membuka dan membaca file " << endl;
+    //jika file ada maka 
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan di sini 
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak di temukan file maka akan menampilkan ini
+    else cout << "unable to open file ";
+    return 0;
+
+}
